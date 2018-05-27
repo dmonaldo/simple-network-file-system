9ffb9e2540345eb08835fa74d6a3d92b7ac76480
Dante Monaldo <dante.monaldo@gmail.com>
2018-05-27 15:52:48 -0700

    Modified formatting

--- server.cpp
+++ server.cpp
@@ -17,25 +17,25 @@ int main(int argc, char* argv[]) {
 
 	if (argc < 2) {
 		cout << "Usage: ./nfsserver port#\n";
-      return -1;
+			return -1;
     }
-    port = atoi(argv[1]);
+		port = atoi(argv[1]);
 		cout << "Connecting to port " << port << endl;
 
 		// create socket
 		sockfd = socket(AF_INET, SOCK_STREAM, 0);
 		if (sockfd < 0) {
-    	perror("ERROR opening socket");
+			perror("ERROR opening socket");
 			exit(0);
 		}
 
 		// bind socket to address and port number
 		bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(port);
-    if (::bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-    	perror("ERROR on bind");
+		serv_addr.sin_family = AF_INET;
+		serv_addr.sin_addr.s_addr = INADDR_ANY;
+		serv_addr.sin_port = htons(port);
+		if (::bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+			perror("ERROR on bind");
 			exit(0);
 		}
 
@@ -56,20 +56,20 @@ int main(int argc, char* argv[]) {
 		cout << "SOCK: " << sockfd << endl;
 		cout << "NEW SOCKET: " << newsockfd << endl;
 
-    // mount the file system
-    FileSys fs;
-    fs.mount(newsockfd); //assume that sock is the new socket created
-                    //for a TCP connection between the client and the server.
+		// mount the file system
+		FileSys fs;
+		fs.mount(newsockfd);
+		//assume that sock is the new socket created
+		//for a TCP connection between the client and the server.
 
-    //loop: get the command from the client and invoke the file
-    //system operation which returns the results or error messages back to the clinet
-    //until the client closes the TCP connection.
+		//loop: get the command from the client and invoke the file
+		//system operation which returns the results or error messages back to the clinet
+		//until the client closes the TCP connection.
 
+		//close the listening socket
 
-    //close the listening socket
+		//unmout the file system
+		fs.unmount();
 
-    //unmout the file system
-    fs.unmount();
-
-    return 0;
+		return 0;
 }
