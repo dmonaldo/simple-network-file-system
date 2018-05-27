ce65903d8e57fc22e5e7ffa0f51c0bd05a648b27
Dante Monaldo <dante.monaldo@gmail.com>
2018-05-27 15:45:04 -0700

    Able to bind, listen and accept requests from client

--- server.cpp
+++ server.cpp
@@ -10,20 +10,17 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	const int BACKLOG = 5;
-	int sockfd, newsockfd, portno, clilen;
+	int sockfd, newsockfd, port, clilen;
 	char buffer[256];
 	struct sockaddr_in serv_addr, cli_addr;
-	socklen_t testMe;
 	int n;
 
 	if (argc < 2) {
 		cout << "Usage: ./nfsserver port#\n";
       return -1;
     }
-    int port = atoi(argv[1]);
-
+    port = atoi(argv[1]);
 		cout << "Connecting to port " << port << endl;
-    //networking part: create the socket and accept the client connection
 
 		// create socket
 		sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -34,28 +31,34 @@ int main(int argc, char* argv[]) {
 
 		// bind socket to address and port number
 		bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = atoi(argv[1]);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_port = htons(port);
     if (::bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-    	perror("ERROR on binding");
+    	perror("ERROR on bind");
 			exit(0);
 		}
 
 		// listen for client to make a connection
-		// int listen(sockfd, BACKLOG);
+		if (::listen(sockfd, BACKLOG) < 0){
+			perror("ERROR on list");
+			exit(0);
+		}
 
 		// accept a request from client
-		// int new_socket = accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+		newsockfd = ::accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t*)&clilen);
+		if (newsockfd < 0) {
+			perror("ERROR on accept");
+			exit(0);
+		}
+		bzero(buffer, 256);
 
-    int sock; //change this line when necessary!
 		cout << "SOCK: " << sockfd << endl;
-		// cout << "NEW SOCKET: " << new_socket << endl;
+		cout << "NEW SOCKET: " << newsockfd << endl;
 
-    //mount the file system
+    // mount the file system
     FileSys fs;
-    fs.mount(sock); //assume that sock is the new socket created
+    fs.mount(newsockfd); //assume that sock is the new socket created
                     //for a TCP connection between the client and the server.
 
     //loop: get the command from the client and invoke the file
