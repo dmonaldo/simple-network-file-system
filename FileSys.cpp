307737c26545ef350ae263a03049a6e66c0dfc3b
WarpFactors <39165491+WarpFactors@users.noreply.github.com>
2018-05-30 15:12:16 -0700

    Revert "mkdir added"

--- FileSys.cpp
+++ FileSys.cpp
@@ -13,8 +13,7 @@ using namespace std;
 // mounts the file system
 void FileSys::mount(int sock) {
   bfs.mount();
-  curr_dir = 1; //by default current directory is home directory,
-  //in disk block #1
+  curr_dir = 1; //by default current directory is home directory, in disk block #1
   fs_sock = sock; //use this socket to receive file system operations from the client and send back response messages
 }
 
@@ -27,65 +26,6 @@ void FileSys::unmount() {
 // make a directory
 void FileSys::mkdir(const char *name)
 {
-  
-  if (strlen(name) > MAX_FNAME_SIZE + 1){
-    cout << "File name is too long.\n";
-    return;
-  }
-
-  // read current directory for duplicate name
-  dirblock_t* curr_block_ptr = new dirblock_t;
-  char file_name[MAX_FNAME_SIZE + 1];
-  char curr_file_name[MAX_FNAME_SIZE + 1];
-  strcpy(file_name, name);
-  bfs.read_block(curr_dir, curr_block_ptr);
-
-  if (curr_block_ptr->num_entries == MAX_DIR_ENTRIES){
-    delete curr_block_ptr;
-    cout << "Directory is full.\n";
-    return;
-  }
-
-  for (unsigned int i = 0; i < MAX_DIR_ENTRIES; i++){
-    strcpy(curr_file_name, curr_block_ptr->dir_entries[i].name);
-
-    if (strcmp(curr_file_name, file_name) == 0){
-      delete curr_block_ptr;
-      cout << "mkdir: cannot create directory '" << name
-           << "â€™: File exists\n";
-      return;
-    }
-  }
-
-  // make new free block 
-  short block_num = bfs.get_free_block();
-  if (block_num == 0){
-    block_num = bfs.get_free_block();
-    if (block_num == 0){
-      cout << "Disk full.\n";
-      delete curr_block_ptr;
-      return;
-    }
-  }
-  
-  //fill new directory block_num to hold 0 to show blocks are unused
-  dirblock_t* new_block = new dirblock_t;
-  new_block->magic = DIR_MAGIC_NUM;
-  new_block->num_entries = 0;
-  for (int i = 0; i < MAX_DIR_ENTRIES; i++){
-    new_block->dir_entries[i].block_num = 0;
-  }
-  bfs.write_block(block_num, new_block);
-  delete new_block;
-
-  strcpy(curr_block_ptr->dir_entries[curr_block_ptr->num_entries].name, name);
-  curr_block_ptr->dir_entries[curr_block_ptr->num_entries].block_num =
-    block_num;
-  curr_block_ptr->num_entries++;
-
-  // write block and delete
-  bfs.write_block(curr_dir, curr_block_ptr);
-  delete curr_block_ptr;
 }
 
 // switch to a directory
@@ -105,23 +45,6 @@ void FileSys::rmdir(const char *name)
 // list the contents of current directory
 void FileSys::ls()
 {
-  //read the data held at the current directory from the disk
-  dirblock_t* curr_block_ptr = new dirblock_t;
-  char curr_file_name[MAX_FNAME_SIZE + 1];
-  bfs.read_block(curr_dir, curr_block_ptr);
-
-  if (curr_block_ptr->num_entries == 0){
-    delete curr_block_ptr;
-    cout << "Directory is empty.\n";
-    return;
-  }
-
-  for (unsigned int i = 0; i < MAX_DIR_ENTRIES; i++){
-    //read each file name entry as long a block for file is not equal to 0
-    //strcpy(curr_file_name, curr_block_ptr->dir_entries[i].name);
-    //put the curr_file_name into socket buffer to send text format to client
-  }
-  delete curr_block_ptr;
 }
 
 // create an empty data file
