d81e568e78a87c104fa6d4c10e49591f657e8a2f
WarpFactors <mattharrison1998@gmail.com>
2018-05-31 13:06:13 -0700

    Matt rough functions

--- FileSys.cpp
+++ FileSys.cpp
@@ -13,8 +13,7 @@ using namespace std;
 // mounts the file system
 void FileSys::mount(int sock) {
   bfs.mount();
-  curr_dir = 1; //by default current directory is home directory,
-  //in disk block #1
+  curr_dir = 1; //by default current directory is home directory, in disk block #1
   fs_sock = sock; //use this socket to receive file system operations from the client and send back response messages
 }
 
@@ -27,7 +26,6 @@ void FileSys::unmount() {
 // make a directory
 void FileSys::mkdir(const char *name)
 {
-  
   if (strlen(name) > MAX_FNAME_SIZE + 1){
     cout << "File name is too long.\n";
     return;
@@ -52,12 +50,12 @@ void FileSys::mkdir(const char *name)
     if (strcmp(curr_file_name, file_name) == 0){
       delete curr_block_ptr;
       cout << "mkdir: cannot create directory '" << name
-           << "â€™: File exists\n";
+           << "’: File exists\n";
       return;
     }
   }
 
-  // make new free block 
+  // make new free block
   short block_num = bfs.get_free_block();
   if (block_num == 0){
     block_num = bfs.get_free_block();
@@ -67,92 +65,117 @@ void FileSys::mkdir(const char *name)
       return;
     }
   }
-  
-  //fill new directory block_num to hold 0 to show blocks are unused
-  dirblock_t* new_block = new dirblock_t;
-  new_block->magic = DIR_MAGIC_NUM;
-  new_block->num_entries = 0;
-  for (int i = 0; i < MAX_DIR_ENTRIES; i++){
-    new_block->dir_entries[i].block_num = 0;
-  }
-  bfs.write_block(block_num, new_block);
-  delete new_block;
 
-  strcpy(curr_block_ptr->dir_entries[curr_block_ptr->num_entries].name, name);
-  curr_block_ptr->dir_entries[curr_block_ptr->num_entries].block_num =
-    block_num;
-  curr_block_ptr->num_entries++;
+  // switch to a directory
+  void FileSys::cd(const char *name)
+  {
+    //retrieve current directory data block
+    dirblock_t* curr_dir_block_ptr = new dirblock_t;
+    bfs.read_block(curr_dir, (void *) &curr_dir_block_ptr);
 
-  // write block and delete
-  bfs.write_block(curr_dir, curr_block_ptr);
-  delete curr_block_ptr;
-}
-
-// switch to a directory
-void FileSys::cd(const char *name)
-{
-}
-
-// switch to home directory
-void FileSys::home() {
-}
-
-// remove a directory
-void FileSys::rmdir(const char *name)
-{
-}
-
-// list the contents of current directory
-void FileSys::ls()
-{
-  //read the data held at the current directory from the disk
-  dirblock_t* curr_block_ptr = new dirblock_t;
-  char curr_file_name[MAX_FNAME_SIZE + 1];
-  bfs.read_block(curr_dir, curr_block_ptr);
-
-  if (curr_block_ptr->num_entries == 0){
-    delete curr_block_ptr;
-    cout << "Directory is empty.\n";
-    return;
+    //check if any sub directories exist in current directory
+    if(curr_dir_block_ptr.num_entries > 0)
+      {
+        //check each sub directory and check directory names for match
+        for(int curr_sub_dir = 1; curr_sub_dir <= curr_dir_block_ptr.num_entries; curr_sub_dir++)
+          {
+            if(strcmp(curr_dir_block_ptr.dir_entries[i].name, name) == 0)
+              {
+                curr_dir = curr_dir_block_ptr.dir_entries[i].block_num;
+                delete curr_dir_block_ptr;
+                return;
+              }
+          }
+      }
+    cout << "cd: No matching directory found for " << name << "\n";
   }
 
-  for (unsigned int i = 0; i < MAX_DIR_ENTRIES; i++){
-    //read each file name entry as long a block for file is not equal to 0
-    //strcpy(curr_file_name, curr_block_ptr->dir_entries[i].name);
-    //put the curr_file_name into socket buffer to send text format to client
+  // switch to home directory
+  void FileSys::home() {
   }
-  delete curr_block_ptr;
-}
 
-// create an empty data file
-void FileSys::create(const char *name)
-{
-}
+  // remove a directory
+  void FileSys::rmdir(const char *name)
+  {
+  }
 
-// append data to a data file
-void FileSys::append(const char *name, const char *data)
-{
-}
+  // list the contents of current directory
+  void FileSys::ls()
+  {
+  }
 
-// display the contents of a data file
-void FileSys::cat(const char *name)
-{
-}
+  // create an empty data file
+  void FileSys::create(const char *name)
+  {
+  }
 
-// display the first N bytes of the file
-void FileSys::head(const char *name, unsigned int n)
-{
-}
+  // append data to a data file
+  void FileSys::append(const char *name, const char *data)
+  {
+  }
 
-// delete a data file
-void FileSys::rm(const char *name)
-{
-}
+  // display the contents of a data file
+  void FileSys::cat(const char *name)
+  {
+    // check if name is too long
+    if (strlen(name) > MAX_FNAME_SIZE + 1){
+      cout << "File name is too long.\n";
+      return;
+    }
+    short cat_file_block_num = 0;
+    datablock_t* cat_file_contents = new datablock_t;
+    dirblock_t* curr_dir_block_ptr = new dirblock_t;
+    bfs.read_block(curr_dir, curr_dir_block_ptr);
+    for(int curr_dir_entry = 0; curr_dir_entry < curr_dir_block_ptr.num_entries; curr_dir_entry++)
+      {
+        if(strcmp(name,curr_dir_block_ptr.dir_entries[i].name)==0)
+          {
+            cat_file_block_num = curr_dir_block_ptr.block_num;
+            bfs.read_block(cat_file_block_num, (void *) &cat_file_contents);
+            cout << cat_file_contents.data << " **end of block** ";
+          }
+      }
+    delete cat_file_contents;
+    delete curr_dir_block_ptr;
+  }
 
-// display stats about file or directory
-void FileSys::stat(const char *name)
-{
-}
+  // display the first N bytes of the file
+  void FileSys::head(const char *name, unsigned int n)
+  {
+    // check if name is too long
+    if (strlen(name) > MAX_FNAME_SIZE + 1)
+      {
+        cout << "File name is too long.\n";
+        return;
+      }
 
-// HELPER FUNCTIONS (optional)
+    // Search for matching filename
 
+    // Find associated inode for file (check for inode_t.magic == INODE_MAGIC_NUM)
+
+    // If file empty, send error message
+
+    // If file has data, check inode for file size in bytes (inode_t.size)
+
+    // If n > inode_t.size print every block in inode_t.blocks[]
+
+    // Else if 0 < n < inode_t.size, check if BLOCK_SIZE|bytes
+    // if n / BLOCK_SIZE is a whole number, print the first n/BLOCK_SIZE blocks
+    // in inode_t.blocks[]
+
+    // if n / BLOCK_SIZE not whole number, print byte by byte (1 byte = 1 char)
+
+  }
+
+
+  // delete a data file
+  void FileSys::rm(const char *name)
+  {
+  }
+
+  // display stats about file or directory
+  void FileSys::stat(const char *name)
+  {
+  }
+
+  // HELPER FUNCTIONS (optional)
