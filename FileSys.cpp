9fd1dbbd6d6a5d68b23356c16da1e5a5cb2203a9
jackie <jackiemhwong@gmail.com>
2018-06-04 22:23:07 -0700

    modified create

--- FileSys.cpp
+++ FileSys.cpp
@@ -246,31 +246,28 @@ void FileSys::create(const char *name)
 {
   char buffer[1024];
   bool error = false;
-  read(fs_sock, buffer, 1024);
-  if(strlen(name) > MAX_FNAME_SIZE + 1){
-    strcpy(buffer, "504: File name is too long\r\n");
-    error = true;
-  }  
-  // read current directory for duplicate name
+
   dirblock_t curr_block_ptr;
   char file_name[MAX_FNAME_SIZE + 1];
   char curr_file_name[MAX_FNAME_SIZE + 1];
   strcpy(file_name, name);
-  strcat(file_name, "\n");
-  bfs.read_block(curr_dir, (void*) &curr_block_ptr);
-  if(!error){ 
-    if(curr_block_ptr.num_entries == MAX_DIR_ENTRIES){
-      strcpy(buffer, "506: Directory is full\r\n");
-      error = true;
-    }
-  }
-  for(unsigned int i = 0; i < MAX_DIR_ENTRIES; i++){
-    strcpy(curr_file_name, curr_block_ptr.dir_entries[i].name);
-    if(strcmp(curr_file_name, file_name) == 0){
-      if(!error){
+  //strcat(file_name, "\n");
+
+  bfs.read_block(curr_dir, (void*)&curr_block_ptr);
+  if(!error){
+    for(unsigned int i = 0; i < MAX_DIR_ENTRIES; i++){
+      strcpy(curr_file_name, curr_block_ptr.dir_entries[i].name);
+      if(strcmp(curr_file_name, file_name) == 0){
         strcpy(buffer, "502: File exists\r\n");
         error = true;
       }
+    }
+  }
+  
+  if(!error){
+    if(strlen(name) > MAX_FNAME_SIZE + 1){
+      strcpy(buffer, "504: File name is too long\r\n");
+      error = true;
     }
   }
 
@@ -285,7 +282,13 @@ void FileSys::create(const char *name)
       }
     }
   }
-
+  if(!error){  
+    if(curr_block_ptr.num_entries == MAX_DIR_ENTRIES){
+      strcpy(buffer, "506: Directory is full\r\n");
+      error = true;
+    }
+  }
+  
   // fill new inode block_num to hold 0 to show blocks are unused
   if(!error){
     inode_t curr_dir_inode;
@@ -302,27 +305,6 @@ void FileSys::create(const char *name)
     curr_block_ptr.dir_entries[curr_block_ptr.num_entries].block_num =
       block_num;
     bfs.write_block(curr_dir, (void*)&curr_block_ptr);
-  }
- 
-  // fill new inode block_num to hold 0 to show blocks are unused
-  if(!error){
-    inode_t curr_dir_inode;
-    curr_dir_inode.magic = INODE_MAGIC_NUM;
-    curr_dir_inode.size = 0;
-    for(int k = 0; k < MAX_DATA_BLOCKS; k++){
-      curr_dir_inode.blocks[k] = 0; // everything unused
-    }
-    // then write to the block
-    bfs.write_block(block_num, (void*) &curr_dir_inode);
-    strcpy(buffer, "200 OK\r\n Length: 0\r\n");
-    strcpy(curr_block_ptr.dir_entries[curr_block_ptr.num_entries].name,
-           name);
-    curr_block_ptr.dir_entries[curr_block_ptr.num_entries].block_num =
-      block_num;
-    curr_block_ptr.num_entries++;
-
-    //write block and delete
-    bfs.write_block(curr_dir, (void*) &curr_block_ptr);
   }
   send(fs_sock, buffer, sizeof(buffer), 0);
 }
